30aedff790fe77c4638f6540d884733be7c75cc1
achintya-cell <achintyasingh.science@gmail.com>
2021-04-09 01:46:55 +0530

    create navbar without drawer

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,110 @@
-import { List, ListItem, makeStyles } from "@material-ui/core";
+import { ListItemText, makeStyles } from "@material-ui/core";
+import InputBase from '@material-ui/core/InputBase';
+import Button  from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { cyan, blueGrey } from '@material-ui/core/colors';
+import {  purple, teal } from '@material-ui/core/colors';
 import { Link } from 'react-router-dom';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import Brightness4Icon from '@material-ui/icons/Brightness4';
+// import FacebookIcon from '@material-ui/icons/Facebook';
+// import TwitterIcon from '@material-ui/icons/Twitter';
+// import InstagramIcon from '@material-ui/icons/Instagram';
+// import Brightness4Icon from '@material-ui/icons/Brightness4';
+import SearchOutlined  from "@material-ui/icons/SearchOutlined";
 
 const useStyles = makeStyles((theme) => {
 	return {
 		navbar: {
-			backgroundColor: blueGrey[50],
+			backgroundColor: purple[500],
 			color: 'white',
+			display: 'flex',
+		},
+		menuIcon: {
+			// display: 'none',
+			transition: theme.transitions.create('opacity',{duration:'0.3s'}),
+			opacity: 0,
+			cursor: 'default',
+			[theme.breakpoints.down('sm')]: {
+				opacity: 1,
+				cursor: 'pointer',
+			}
 		},
 		title: {
+			// flexGrow: 1,
+			// marginLeft: '10px',
+			// marginRight: '20px',
+		},
+		header: {
+			display: 'flex',
 			flexGrow: 1,
-			marginLeft: theme.spacing(5),
+			marginLeft: '-10px',
+		},
+		headerList: {
+			display: 'flex',
+			[theme.breakpoints.down('md')]: {
+				marginRight: '-92px',
+			},
+		},
+		headerLinksText: {
+			textDecoration: 'none',
+			color: 'white',
+			[theme.breakpoints.down('sm')]: {
+				display: 'none',
+			},
 		},
 		whiteColor: {
 			color: 'white',
 		},
 		navList: {
 			display: 'flex',
-		}
+		},
+		navLinks: {
+			borderColor: 'white',
+			marginRight: '-24px',
+		},
+		navLinksText: {
+			textDecoration: 'none',
+			color: 'white',
+		},
+		searchBar: {
+			 backgroundColor: 'black',
+			 display: 'flex',
+			 borderRadius: theme.shape.borderRadius,
+			 height: '32px',
+			 width: 'auto',
+			 marginRight: '10px',
+			 [theme.breakpoints.down('md')]: {
+				 marginRight: '-8px',
+				//  display: 'none',
+			 },
+			 [theme.breakpoints.down('xs')]: {
+				 display: 'none',
+			 },
+		},
+		searchIcon: {
+			padding: theme.spacing(0.5,2),
+			// height: '10px',
+			// position: 'absolute',
+			pointerEvents: 'none',
+			// display: 'flex',
+			// alignItems: 'center',
+			// justifyContent: 'center',
+		},
+		searchInput: {
+			padding: theme.spacing(2,0),
+			transition: theme.transitions.create('width'),
+			width: '19ch',
+			[theme.breakpoints.up('md')]: {
+				width: '30ch',
+				'&:focus': {
+				  width: '40ch',
+				},
+			},
+		},
 	}
 })
 
@@ -43,18 +121,45 @@ const NavBar = () => {
 			>
 				<Toolbar>
 					{/* Navigation Icon */}
-					<IconButton edge="start">
+					<IconButton edge="start" className={classes.menuIcon}>
 						<MenuIcon className={classes.whiteColor} />
 					</IconButton>
 
 					{/* Title */}
-					<Typography className={classes.title}>
-						The Alimento
-					</Typography>
+					<div className={classes.header}>
+						<List className={classes.headerList}>
+							<ListItem>
+								<Typography className={classes.title} noWrap variant="h5">
+								Alimento
+								</Typography>
+							</ListItem>
+							<ListItem>
+								<Link to="/" className={classes.headerLinksText}>Home</Link>
+							</ListItem>
+							<ListItem>
+								<Link to="/" className={classes.headerLinksText}>About</Link>
+							</ListItem>
+							<ListItem>
+								<Link to="/" className={classes.headerLinksText}>Menu</Link>
+							</ListItem>
+						</List>
+					</div>
 
-					{/* Social Links */}
+					{/* Search Bar */}
+					<div className={classes.searchBar}>
+						<div className={classes.searchIcon}>
+							<SearchOutlined />
+						</div>
+						<InputBase
+							placeholder="Search…"
+							className={classes.searchInput}
+							inputProps={{ 'aria-label': 'search' }}
+						/>
+					</div>
+
+					{/* Buttons */}
 					<List className={classes.navList}>
-						<ListItem>
+						{/* <ListItem>
 							<Link to="/" className={classes.whiteColor}>
 								<Brightness4Icon />
 							</Link>
@@ -73,6 +178,16 @@ const NavBar = () => {
 							<Link to="/" className={classes.whiteColor}>
 								<InstagramIcon />
 							</Link>
+						</ListItem> */}
+						<ListItem>
+							<Button variant="outlined" className={classes.navLinks}>
+								<Link to="/" className={classes.navLinksText}>SignUp</Link>
+							</Button>
+						</ListItem>
+						<ListItem>
+								<Button variant="contained">
+									<Link to="/" className={classes.navLinksText}>Login</Link>
+								</Button>
 						</ListItem>
 					</List>
 
