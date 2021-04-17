6fa7c7176157771cd48f10d63650a37eb46c99ba
Aman Mishra <ashu18aman@gmail.com>
2021-04-17 18:45:42 +0530

    add carousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import NavBar from "./components/NavBar";
+import MyCarousel from "./components/MyCarousel";
+import "bootstrap/dist/css/bootstrap.css";
 
 const theme = createMuiTheme({
   
@@ -13,6 +15,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <Router>
         <NavBar />
+        <MyCarousel />
       </Router>
     </ThemeProvider>
   );
