653804c387eb0f4fe3416eabc535ec7a88a75842
Aditya Yadav <adityakumaryadav21102001@gmail.com>
2021-05-03 01:40:53 +0530

    Instant_order

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import MyCarousel from "./components/MyCarousel";
 import "bootstrap/dist/css/bootstrap.css";
 import Featured from "./components/Featured_dishes"
+import Instant_order from "./components/Instant_order"
 
 const theme = createMuiTheme({
   
@@ -18,6 +19,7 @@ function App() {
         <NavBar />
         <MyCarousel />
         <Featured />
+        <Instant_order />
       </Router>
     </ThemeProvider>
   );
---
+++ src/components/Instant_order.js
@@ -0,0 +1,77 @@
+import { Container, Grid } from '@material-ui/core';
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import { FormControl } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { findByLabelText } from '@testing-library/dom';
+import purple from '@material-ui/core/colors/purple';
+
+const useStyles = makeStyles({
+    root: { 
+        marginTop: 15,
+        width: '100%',
+    },
+    grd:{
+        marginTop: 25,
+    },
+    
+});
+
+const Instant_order = () => {
+    const classes = useStyles();
+    return (
+        <Container className="root">
+         <Typography variant="h3" component="h3" align="center" gutterBottom>
+                INSTANT ORDER
+            </Typography>
+            <Grid container direction="row"
+                justify="center"
+                alignItems="center">
+                <Grid item>
+                    <Form>
+                        <FormControl
+                        color="secondary"
+                        >
+                            <TextField
+                                margin="normal"
+                                id="outlined-multiline-static"
+                                label="Good Name"
+                                multiline
+                                rows={4}
+                                defaultValue="Default Value"
+                                variant="outlined"
+                                className="grd"
+                            />
+                            <TextField
+                                margin="normal"
+                                id="outlined-multiline-static"
+                                label="Email"
+                                multiline
+                                rows={4}
+                                defaultValue="Default Value"
+                                variant="outlined"
+                                className="grd"
+                            />
+                            <TextField
+                                margin="normal"
+                                id="outlined-multiline-static"
+                                label="Address"
+                                multiline
+                                rows={4}
+                                defaultValue="Default Value"
+                                variant="outlined"
+                                className="grd"
+                            />
+
+                        </FormControl>
+                    </Form>
+                </Grid>
+            </Grid>
+
+        </Container>
+    );
+}
+
+export default Instant_order;
