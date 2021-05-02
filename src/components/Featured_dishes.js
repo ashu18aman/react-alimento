d2938e9d29e542b78ddcdc5dda496a0e71308843
Aditya Yadav <adityakumaryadav21102001@gmail.com>
2021-05-03 00:02:27 +0530

    featured_dishes_1

--- src/components/Featured_dishes.js
+++ src/components/Featured_dishes.js
@@ -4,11 +4,16 @@ import ReactDOM from 'react-dom';
 import RecipeReviewCard from './Card';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles({
     root: {
         marginTop: 15,
         width: '100%',
+    },
+    add:{
+        height: 400,
+        width: 400,
     },
 });
 
@@ -30,6 +35,19 @@ const Featured = () => {
                 </Grid>
                 <Grid item>
                     <RecipeReviewCard />
+                </Grid>
+                <Grid item>
+                    <RecipeReviewCard />
+                </Grid>
+                <Grid item>
+                    <RecipeReviewCard />
+                </Grid>
+                <Grid item>
+                    <AddIcon fontSize="large" className={classes.add} />
+                    <Typography variant="h4" component="h4" align="center" gutterBottom>
+                More 
+            </Typography>
+                    
                 </Grid>
             </Grid>
         </Container>
