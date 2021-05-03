225338ea7073ef6bc6515c49c87796dda0127ffa
Aditya Yadav <adityakumaryadav21102001@gmail.com>
2021-05-03 14:03:56 +0530

    Instant_order_submit

--- src/components/InstantOrder.js
+++ src/components/InstantOrder.js
@@ -5,13 +5,17 @@ import { FormControl } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+import { CenterFocusStrong } from '@material-ui/icons';
+
 
 const useStyles = makeStyles({
     root: {
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
         marginTop: 15,
         marginBottom: 15,
-        padding:15,
+        padding: 15,
         width: '100%',
     },
     grd: {
@@ -20,6 +24,14 @@ const useStyles = makeStyles({
     textInp: {
         width: 380,
         background: 'linear-gradient(45deg, #e8eaf6 30%, #e8eaf6 90%)',
+    },
+    sub: {
+        width:'100%',
+        padding: 15,
+        marginTop: 15,
+        background: 'linear-gradient(45deg, #ffecb3 30%, #ffecb3 90%)',
+        justify:CenterFocusStrong,
+        color: '#212121',
     },
 
 });
@@ -33,7 +45,7 @@ const InstantOrder = () => {
                     Check out our today's surprise meal !!!
                 </Typography>
                 <Typography variant="h5" component="h3" align="center" gutterBottom>
-                    Fill the below form and we will contact you shortly.... 
+                    Fill the below form and we will contact you shortly....
                 </Typography>
             </div>
             <Grid container direction="row"
@@ -49,23 +61,31 @@ const InstantOrder = () => {
                                 justify="center"
                                 alignItems="center"
                                 spacing={5}>
-                            <Grid item>
-                            <TextField id="outlined-basic" label="Name" variant="outlined" className={classes.textInp} />
+                                <Grid item>
+                                    <TextField id="outlined-basic" label="Name" variant="outlined" className={classes.textInp} />
                                 </Grid>
                                 <Grid item>
-                                <TextField id="outlined-basic" label="Email" variant="outlined" className={classes.textInp} />
+                                    <TextField id="outlined-basic" label="Email" variant="outlined" className={classes.textInp} />
                                 </Grid>
                                 <Grid item>
-                                <TextField id="outlined-basic" label="Address" variant="outlined" className={classes.textInp} />
+                                    <TextField id="outlined-basic" label="Address" variant="outlined" className={classes.textInp} />
                                 </Grid>
                             </Grid>
-
                         </FormControl>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            className={classes.sub}
+                            endIcon={<SendOutlinedIcon />}
+
+                        >
+                            Order Now
+      </Button>
                     </Form>
                 </Grid>
             </Grid>
 
-        </Container>
+        </Container >
     );
 }
 
