95229b9e8bfb2bcaea4db8f49a0d6fcc78e2af37
Aditya Yadav <adityakumaryadav21102001@gmail.com>
2021-05-03 13:15:21 +0530

    Instant_order_update_3

--- src/components/InstantOrder.js
+++ src/components/InstantOrder.js
@@ -8,7 +8,10 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
     root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
         marginTop: 15,
+        marginBottom: 15,
+        padding:15,
         width: '100%',
     },
     grd: {
@@ -20,7 +23,7 @@ const useStyles = makeStyles({
 const InstantOrder = () => {
     const classes = useStyles();
     return (
-        <Container className="root">
+        <Container className={classes.root}>
             <div>
                 <Typography variant="h3" component="h3" align="center" gutterBottom>
                     Check out our today's surprise meal !!!
