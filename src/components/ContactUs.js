b04b50012bc24d8d370673aaebdb314fd04f6e95
Aditya Yadav <adityakumaryadav21102001@gmail.com>
2021-05-04 09:43:02 +0530

    initial_contact_us

---
+++ src/components/ContactUs.js
@@ -0,0 +1,22 @@
+import { Container, Grid } from '@material-ui/core';
+import React from 'react';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField'
+
+const Contact = () => {
+    return (
+        <Container>
+            <ContactMailIcon />
+            <Grid container direction="row"
+                justify="center"
+                alignItems="center">
+                <Grid item xs={6} >
+                <TextField id="Name" label="Full Name" variant="outlined" />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+}
+
+export default Contact;
